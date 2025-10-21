07eb6a5cf37c8a3ca485920e238fbb1301cb6631
496Menesses <acmenesess@unicauca.edu.co>
2025-10-21 00:09:27 -0500

    Config docker

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, name, email, created_at) VALUES (1, 'Alice', 'alice@example.com', CURRENT_TIMESTAMP);
+INSERT INTO users (id, name, email, created_at) VALUES (2, 'Bob',   'bob@example.com',   CURRENT_TIMESTAMP);
