ecd5c48bda8402cbaf31f5abf00ba8de0bb7f57a
Andres Atehortua <andres.atehortua@loanlogics.com>
2025-10-23 21:58:04 -0500

    disable test

--- src/test/java/com/sabana/GestionUsuariosApplicationTests.java
+++ src/test/java/com/sabana/GestionUsuariosApplicationTests.java
@@ -1,8 +1,10 @@
 package com.sabana;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class GestionUsuariosApplicationTests {
 
